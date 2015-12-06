1e454e28a58aecca1eaf3f358b530c2070c7a1bb
Jess Zanewich <jessica.zanewich@gmail.com>
2015-12-05 21:02:34 -0800

    Figured out how to display sprite. Possibly adding angular to the project.

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $.getJSON("http://pokeapi.co/api/v1/pokedex/1/", function(response){
     console.log(response);
   });
+  retrieveSprite();
+  function retrieveSprite() {
+    $.getJSON("http://pokeapi.co/api/v1/sprite/1/", function(data){
+      $(".sprite").html("<img src=http://pokeapi.co" + data.image + ">");
+
+    });
+  };
+
 });

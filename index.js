46ecd737e2ced5eed7b7e1ef43d3001c100d5b6a
Jess Zanewich <jessica.zanewich@gmail.com>
2015-12-07 17:48:36 -0800

    Setup Angular in the project. Created a controller and service to make sure the retrieval to external API worked properly.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $.getJSON("http://pokeapi.co/api/v1/pokedex/1/", function(response){
     console.log(response);
   });
@@ -10,4 +10,32 @@ $(document).ready(function() {
     });
   };
 
-});
+});*/
+
+
+angular.module("PokeAnalysis", [])
+.factory("PokeService", ["$http", function($http) {
+  return {
+    getPokeList: function() {
+      var pokemon = [];
+      return $http.get("http://pokeapi.co/api/v1/pokedex/1/")
+        .success(function(response) {
+          console.log(response + "Found");
+          pokemon = response;
+        }).error(function() {
+          console.log("Error with pokemon retrieval");
+        });
+    }
+  };
+}])
+
+.controller("PokemonController", ['$scope', "PokeService", function($scope, PokeService) {
+
+  $scope.pokemonList = function() {
+    PokeService.getPokeList().then(
+      function(pokemon){
+        $scope.pokemon = pokemon;
+        console.log(pokemon);
+      });
+  };
+}]);
